fd65ee8951f6013a807dc224a123992b10555787
Ben March <24650490+bamarch@users.noreply.github.com>
2021-01-09 22:11:53 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Bancrates The Eternal</h1>
+    <p>I set out on a mission to ban all crates.</p>
+    <p>I succeeded only in banning myself.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
